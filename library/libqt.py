d1616415846a75723e92ad0088f243281d7694ca
hrpzcf <hrpzcf@foxmail.com>
2021-01-10 16:59:14 +0800

    fix: libqt中QTextEditMod类接受文件拖拽时拖入文件夹自动搜索其中文件

--- library/libqt.py
+++ library/libqt.py
@@ -13,7 +13,8 @@ class QLineEditMod(QLineEdit):
         super().__init__()
         self.setContextMenuPolicy(Qt.NoContextMenu)
         self._accept = accept
-        self._file_filter = file_filter
+        self._filter = file_filter
+        self._drag_temp = ''
 
     @property
     def local_path(self):
@@ -22,10 +23,12 @@ class QLineEditMod(QLineEdit):
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls():
             if self._accept == 'file':
-                path = event.mimeData().urls()[0].toLocalFile()
+                self._drag_temp = os.path.realpath(
+                    event.mimeData().urls()[0].toLocalFile()
+                )
                 if (
-                    not self._file_filter
-                    or os.path.splitext(path)[1] in self._file_filter
+                    not self._filter
+                    or os.path.splitext(self._drag_temp)[1] in self._filter
                 ):
                     event.accept()
                 else:
@@ -38,19 +41,24 @@ class QLineEditMod(QLineEdit):
             event.ignore()
 
     def dropEvent(self, event):
-        path = event.mimeData().urls()[0].toLocalFile()
-        if self._accept == 'file' and os.path.isfile(path):
-            self.setText(path)
-        elif self._accept == 'dir' and os.path.isdir(path):
-            self.setText(path)
+        if not self._drag_temp:
+            self._drag_temp = os.path.realpath(
+                event.mimeData().urls()[0].toLocalFile()
+            )
+        if self._accept == 'file' and os.path.isfile(self._drag_temp):
+            self.setText(self._drag_temp)
+        elif self._accept == 'dir' and os.path.isdir(self._drag_temp):
+            self.setText(self._drag_temp)
 
 
 class QTextEditMod(QTextEdit):
     def __init__(self, accept='file', file_filter=set()):
         super().__init__()
+        self.setLineWrapMode(QTextEdit.NoWrap)
         self.setContextMenuPolicy(Qt.NoContextMenu)
         self._accept = accept
-        self._file_filter = file_filter
+        self._filter = file_filter
+        self._drag_temp = list()
 
     @property
     def local_paths(self):
@@ -61,13 +69,29 @@ class QTextEditMod(QTextEdit):
             return [path for path in file_dir_paths if os.path.isfile(path)]
         return []
 
+    def _stash_from_urls(self, urls):
+        self._drag_temp.clear()
+        for file_or_dir in (path.toLocalFile() for path in urls):
+            file_or_dir = os.path.realpath(file_or_dir)
+            if os.path.isfile(file_or_dir):
+                self._drag_temp.append(file_or_dir)
+                continue
+            self._drag_temp.append(file_or_dir)
+            for root, _, files in os.walk(file_or_dir):
+                self._drag_temp.extend(
+                    os.path.join(root, filename) for filename in files
+                )
+
     def dragEnterEvent(self, event):
+        self._drag_temp.clear()
         if event.mimeData().hasUrls():
             if self._accept == 'file':
-                if not self._file_filter or set(
-                    os.path.splitext(path.toLocalFile())[1]
-                    for path in event.mimeData().urls()
-                ).issubset(self._file_filter):
+                self._stash_from_urls(event.mimeData().urls())
+                if not self._filter or set(
+                    os.path.splitext(fp)[1]
+                    for fp in self._drag_temp
+                    if os.path.isfile(fp)
+                ).issubset(self._filter):
                     event.accept()
                 else:
                     event.ignore()
@@ -83,18 +107,21 @@ class QTextEditMod(QTextEdit):
     def dropEvent(self, event):
         cur_text = self.toPlainText()
         super().dropEvent(event)
-        files_dirs = (path.toLocalFile() for path in event.mimeData().urls())
+        if not self._drag_temp:
+            self._stash_from_urls(event.mimeData().urls())
         if self._accept == 'file':
             self.setText(
                 cur_text
                 + '\n'.join(
-                    path for path in files_dirs if os.path.isfile(path)
+                    path for path in self._drag_temp if os.path.isfile(path)
                 )
             )
         elif self._accept == 'dir':
             self.setText(
                 cur_text
-                + '\n'.join(path for path in files_dirs if os.path.isdir(path))
+                + '\n'.join(
+                    path for path in self._drag_temp if os.path.isdir(path)
+                )
             )
         else:
             self.setText('')
