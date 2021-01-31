da02a952d79d68938c173ad6ea0b25140e1fb2f0
hrpzcf <hrpzcf@foxmail.com>
2021-01-31 15:24:36 +0800

    refactor: 清理interface无用代码

--- interface/__init__.py
+++ interface/__init__.py
@@ -1,18 +1,15 @@
 # coding: utf-8
 
-from interface.InformationPanel import Ui_InformationPanel
-from interface.MainInterface import Ui_MainInterface
-from interface.PackageManager import Ui_PackageManager
-from interface.MirrorSourceManager import Ui_MirrorSourceManager
-from interface.PyInstallerTool import Ui_PyInstallerTool
-from interface.PyiToolChoosePyEnv import Ui_PyiToolChoosePyEnv
+from .MainInterface import Ui_MainInterface
+from .PackageManager import Ui_PackageManager
+from .MirrorSourceManager import Ui_MirrorSourceManager
+from .PyInstallerTool import Ui_PyInstallerTool
+from .PyiToolChoosePyEnv import Ui_PyiToolChoosePyEnv
 
 __all__ = [
-    'Ui_InformationPanel',
     'Ui_MainInterface',
     'Ui_PackageManager',
     'Ui_MirrorSourceManager',
     'Ui_PyInstallerTool',
-    'Ui_PyiToolChoosePyEnv'
-
+    'Ui_PyiToolChoosePyEnv',
 ]
