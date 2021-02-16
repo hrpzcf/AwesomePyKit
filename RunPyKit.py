3a35522677a893bf8de645d20022f6a1435ec3f9
hrpzcf <hrpzcf@foxmail.com>
2021-02-17 00:29:32 +0800

    release: 0.4.0

--- RunPyKit.py
+++ RunPyKit.py
@@ -63,7 +63,7 @@ from library.libm import PyEnv
 from library.libpyi import PyiTool
 from library.libqt import QLineEditMod, QTextEditMod
 
-PYKIT_VERSION = '0.3.3'
+PYKIT_VERSION = '0.4.0'
 
 
 class MainInterfaceWindow(Ui_MainInterface, QMainWindow):
