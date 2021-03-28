02560029eda9c8345b8801cd26dd3f76c1f917aa
hrpzcf <hrpzcf@foxmail.com>
2021-03-28 18:38:03 +0800

    feat: interface增加download_package和install_package界面

--- interface/__init__.py
+++ interface/__init__.py
@@ -2,7 +2,9 @@
 
 from .check_imports import Ui_check_imports
 from .choose_env import Ui_choose_env
+from .download_package import Ui_download_package
 from .index_url_manager import Ui_index_url_manager
+from .install_package import Ui_install_package
 from .main_interface import Ui_main_interface
 from .package_manager import Ui_package_manager
 from .pyinstaller_tool import Ui_pyinstaller_tool
@@ -10,7 +12,9 @@ from .pyinstaller_tool import Ui_pyinstaller_tool
 __all__ = [
     "Ui_check_imports",
     "Ui_choose_env",
+    "Ui_download_package",
     "Ui_index_url_manager",
+    "Ui_install_package",
     "Ui_main_interface",
     "Ui_package_manager",
     "Ui_pyinstaller_tool",
