e14c3f614bc13bf872ef35d1199cb617637d8a35
hrpzcf <hrpzcf@foxmail.com>
2021-04-24 21:47:15 +0800

    release: 0.6.3

--- information.py
+++ information.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 NAME = "AwesomePyKit"
-VERSION = "0.6.2"
+VERSION = "0.6.3"
 AUTHOR = "hrp/hrpzcf"
 EMAIL = "hrpzcf@foxmail.com"
 WEBSITE = "https://gitee.com/hrpzcf/AwesomePyKit"
