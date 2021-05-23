57827782b150f596d323bab910702cacc6607443
hrpzcf <hrpzcf@foxmail.com>
2021-05-23 23:09:14 +0800

    release: 0.8.0

--- information.py
+++ information.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 
 NAME = "AwesomePyKit"
-VERSION = "0.7.2"
+VERSION = "0.8.0"
 AUTHOR = "hrp/hrpzcf"
 EMAIL = "hrpzcf@foxmail.com"
 WEBSITE = "https://gitee.com/hrpzcf/AwesomePyKit"
+
+req_fastpip = 0, 11, 5
