6bf29ac5404224ccb1d853e8baa3b80b8732aa40
hrpzcf <hrpzcf@foxmail.com>
2022-01-05 11:56:04 +0800

    release: 0.8.2

--- information.py
+++ information.py
@@ -1,9 +1,9 @@
 # coding: utf-8
 
 NAME = "AwesomePyKit"
-VERSION = "0.8.1"
+VERSION = "0.8.2"
 AUTHOR = "hrp/hrpzcf"
 EMAIL = "hrpzcf@foxmail.com"
 WEBSITE = "https://gitee.com/hrpzcf/AwesomePyKit"
 
-req_fastpip = 0, 12, 0  # 目前没用上
+req_fastpip = 0, 12, 1  # 目前没用上
