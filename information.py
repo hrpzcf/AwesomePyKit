c43c23a2e395af80c44d0ee6f05adcb94e036106
hrpzcf <pphrp@qq.com>
2022-09-05 21:55:33 +0800

    版本：0.9.1-Beta

--- information.py
+++ information.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 NAME = "AwesomePyKit"
-VERSION = "0.9.0-beta"
+VERSION = "0.9.1-Beta"
 AUTHOR = "hrp/hrpzcf"
 EMAIL = "hrpzcf@foxmail.com"
 WEBSITE = "https://gitee.com/hrpzcf/AwesomePyKit"
