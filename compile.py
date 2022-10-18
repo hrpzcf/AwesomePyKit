6972ad9cfe912759301aadef32bfa47b3f6a1182
hrpzcf <pphrp@qq.com>
2022-10-18 12:09:29 +0800

    修正 ui 文件批量转换脚本

--- compile.py
+++ compile.py
@@ -1,19 +1,17 @@
 # coding: utf-8
 
-__doc__ = "将 QtDesigner 生成的 ui 文件批量编译为 py 文件。"
+# 将 QtDesigner 生成的 .ui 文件批量转换为 .py 文件
 
 import os
 import sys
 
-ui_dir_path = "./source/ui"
-compile_cmd = "pyuic5 {} -o {}"
+compile_cmd = "pyuic5 -o {} {}"
+ui_dir_path = "source/awespykit/ui"
 
 
 def compile_ui(any_path):
-    """将 any_path 目录中所有 ui 文件编译为 py 文件。"""
     current_dir = os.getcwd()
     try:
-        # 切换工作目录的原因是防止生成的 py 文件注释中出现 ui 文件的相对或完整路径
         os.chdir(any_path)
         ui_list = [f for f in os.listdir() if f.endswith(".ui")]
     except Exception:
@@ -24,14 +22,14 @@ def compile_ui(any_path):
 
     for ui_name in ui_list:
         src_name = os.path.splitext(ui_name)[0] + ".py"
-        print(f"编译目标：<{ui_name:<{ui_name_max}}>")
+        print(f"转换目标：<{ui_name:<{ui_name_max}}>")
         try:
-            os.system(compile_cmd.format(ui_name, src_name))
-            print(f"编译完成...\n")
+            os.system(compile_cmd.format(src_name, ui_name))
+            print(f"转换完成...\n")
         except Exception:
-            print(f"文件 <{ui_name}> 编译失败，请检查是否已安装 PyQt5...\n")
+            print(f"文件 <{ui_name}> 转换失败，请检查是否已安装 PyQt5...\n")
             break
-    print("编译结束...")
+    print("转换结束...")
     os.chdir(current_dir)
 
 
