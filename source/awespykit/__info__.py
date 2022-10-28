86aa18d1b1d8662c7aafd52aa2265df108fcfe02
hrpzcf <pphrp@qq.com>
2022-10-28 15:55:45 +0800

    版本：1.1.0

--- source/awespykit/__info__.py
+++ source/awespykit/__info__.py
@@ -3,6 +3,6 @@
 __all__ = ["NAME", "VERSION", "AUTHOR", "URL"]
 
 NAME = "Awespykit"
-VERSION = "1.0.0"
-AUTHOR = "hrpzcf hrp"
+VERSION = "1.1.0"
+AUTHOR = "hrpzcf / hrp"
 URL = "https://github.com/hrpzcf/AwesomePyKit"
